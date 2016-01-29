2ab216ba2c3fb3f5b9d56a3c214b8e574f38480c
unknown <miguel.meza@cadempc1.cademsmart.cl>
2016-01-29 15:05:09 -0300

    Versión estable para producción

--- app/routes/TodosRoute.js
+++ app/routes/TodosRoute.js
@@ -9,8 +9,10 @@ let express = require('express');
 let Controller = autoload('./app/controllers/TodosController');
 let router = express.Router();
 
-router.get('/', Controller.list);
-router.post('/', Controller.create);
-router.get('/test', Controller.index);
+router.get('/', Controller.list); // GET  /todos
+router.get('/:id', Controller.find); // GET  /todos/:id
+router.post('/', Controller.create); // POST /todos
+router.put('/:id', Controller.update); // PUT /todos/:id
+router.delete('/:id', Controller.destroy); // DELETE /todos/:id
 
 module.exports = router;
